945a2c99cd3c4fec0d49a33fb7007600481f00a9
luisreis <lreis@stackstate.com>
2018-04-16 14:36:08 +0200

    Implement AkkaHTTPWebContext

--- src/main/scala/com/stackstate/pac4j/AkkaHttpWebContext.scala
+++ src/main/scala/com/stackstate/pac4j/AkkaHttpWebContext.scala
@@ -4,46 +4,116 @@ import java.util
 
 import akka.http.scaladsl.model.HttpRequest
 import org.pac4j.core.context.{ Cookie, WebContext }
+import scala.collection.JavaConverters._
 
+/**
+ * The AkkaHttpWebContext is responsible for wrapping an HTTP request and stores changes that are produced by pac4j and
+ * need to be applied to an HTTP response.
+ */
 case class AkkaHttpWebContext(request: HttpRequest) extends WebContext {
+  import com.stackstate.pac4j.AkkaHttpWebContext._
 
-  override def getRequestCookies: util.Collection[Cookie] = ???
+  private var changes = ResponseChanges.empty
 
-  override def addResponseCookie(cookie: Cookie): Unit = ???
+  override def getRequestCookies: util.Collection[Cookie] = {
+    request.cookies.map { akkaCookie =>
+      new Cookie(akkaCookie.name, akkaCookie.value)
+    }.asJavaCollection
+  }
+
+  override def addResponseCookie(cookie: Cookie): Unit = {
+    changes = changes.copy(cookies = changes.cookies ++ List(cookie))
+  }
 
   override def getSessionStore = ???
 
-  override def getRemoteAddr: String = ???
+  override def getRemoteAddr: String = {
+    request.getUri().getHost.address()
+  }
 
-  override def setResponseHeader(name: String, value: String): Unit = ???
+  override def setResponseHeader(name: String, value: String): Unit = {
+    changes = changes.copy(headers = changes.headers ++ List((name, value)))
+  }
 
-  override def getRequestParameters: util.Map[String, Array[String]] = ???
+  override def getRequestParameters: util.Map[String, Array[String]] = {
+    request.getUri().query().toMap.asScala.mapValues(Array(_)).asJava
+  }
 
-  override def getFullRequestURL: String = ???
+  override def getFullRequestURL: String = {
+    request.getUri().getScheme + "://" + request.getUri().getHost.address() + request.getUri().getPathString
+  }
 
-  override def getServerName: String = ???
+  override def getServerName: String = {
+    request.getUri().host.address().split(":")(0)
+  }
 
-  override def setResponseContentType(content: String): Unit = ???
+  override def setResponseContentType(contentType: String): Unit = {
+    changes = changes.copy(contentType = contentType)
+  }
 
-  override def writeResponseContent(content: String): Unit = ???
+  override def writeResponseContent(content: String): Unit = {
+    if (content != null) {
+      changes = changes.copy(content = changes.content + content)
+    } else ()
+  }
 
-  override def getPath: String = ???
+  override def getPath: String = {
+    request.getUri().path
+  }
 
-  override def setResponseStatus(code: Int): Unit = ???
+  override def setResponseStatus(code: Int): Unit = ()
 
-  override def getRequestParameter(name: String): String = ???
+  override def getRequestParameter(name: String): String = {
+    request.getUri().query().getOrElse(name, "")
+  }
 
-  override def getRequestHeader(name: String): String = ???
+  override def getRequestHeader(name: String): String = {
+    request.headers.find(_.name() == name).map(_.value).getOrElse("")
+  }
 
-  override def getScheme: String = ???
+  override def getScheme: String = {
+    request.getUri().getScheme
+  }
 
-  override def isSecure: Boolean = ???
+  override def isSecure: Boolean = {
+    val scheme = request.getUri().getScheme.toLowerCase
 
-  override def getRequestMethod: String = ???
+    scheme == "https" || scheme == "ldap"
+  }
 
-  override def getServerPort: Int = ???
+  override def getRequestMethod: String = {
+    request.method.value
+  }
 
-  override def setRequestAttribute(name: String, value: scala.AnyRef): Unit = ???
+  override def getServerPort: Int = {
+    request.getUri().getPort
+  }
 
-  override def getRequestAttribute(name: String): AnyRef = ???
+  override def setRequestAttribute(name: String, value: scala.AnyRef): Unit = {
+    changes = changes.copy(attributes = changes.attributes ++ Map[String, AnyRef](name -> value))
+  }
+
+  override def getRequestAttribute(name: String): AnyRef = {
+    changes.attributes.getOrElse(name, "")
+  }
+
+  def getChanges: ResponseChanges = changes
+}
+
+object AkkaHttpWebContext {
+
+  //This class is where all the HTTP response changes are stored so that they can later be applied to an HTTP Request
+  case class ResponseChanges(
+    headers: List[(String, String)],
+    contentType: String,
+    content: String,
+    cookies: List[Cookie],
+    attributes: Map[String, AnyRef])
+
+  object ResponseChanges {
+    def empty: ResponseChanges = {
+      ResponseChanges(List.empty, "", "", List.empty, Map.empty)
+    }
+  }
+
 }
--- src/test/scala/com/stackstate/pac4j/AkkaHttpWebContextTest.scala
+++ src/test/scala/com/stackstate/pac4j/AkkaHttpWebContextTest.scala
@@ -3,61 +3,65 @@ package com.stackstate.pac4j
 import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
 import akka.http.scaladsl.model.headers.Cookie
 import akka.http.scaladsl.model.{ HttpHeader, HttpRequest, Uri }
-import org.pac4j.core.context.{ Cookie => PCookie }
 import org.scalatest.{ Matchers, WordSpecLike }
+import scala.collection.JavaConverters._
 
 class AkkaHttpWebContextTest extends WordSpecLike with Matchers {
-  lazy val cookie = new PCookie("cookieName", "cookieValue")
+  lazy val cookie = ("cookieName", "cookieValue")
 
   "AkkaHttpWebContext" should {
-    "be able to get/set request cookies" in withContext(cookies = List(Cookie("cookieName", "cookieValue"))) { webContext =>
+    "get/set request cookies" in withContext(cookies = List(Cookie("cookieName", "cookieValue"))) { webContext =>
       val contextCookie = webContext.getRequestCookies
-      contextCookie shouldEqual Array(cookie)
+      contextCookie.asScala.map(cookie => (cookie.getName, cookie.getValue)) shouldEqual Seq(cookie)
     }
 
-    "be able to get a request header or an empty string when no such header exists" in withContext(requestHeaders = List(("foo", "bar"))) { webContext =>
+    "get a request header or an empty string when no such header exists" in withContext(requestHeaders = List(("foo", "bar"))) { webContext =>
       webContext.getRequestHeader("foo") shouldEqual "bar"
       webContext.getRequestHeader("abc") shouldEqual ""
     }
 
-    "be able to get the full url from a request" in withContext(url = "www.stackstate.com/views") { webContext =>
+    "get the full url from a request" in withContext(url = "www.stackstate.com/views") { webContext =>
       webContext.getFullRequestURL shouldEqual "http://www.stackstate.com/views"
     }
 
-    "be able to get the request path" in withContext(url = "www.stackstate.com/views") { webContext =>
-      webContext.getPath shouldEqual "views"
+    "get the request path" in withContext(url = "www.stackstate.com/views") { webContext =>
+      webContext.getPath shouldEqual "www.stackstate.com/views"
     }
 
-    "be able to get the remote address from a request" in withContext(url = "www.stackstate.com/views", hostAddress = "localhost:8000") { webContext =>
-      webContext.getRemoteAddr shouldEqual "localhost:8000"
+    "get the remote address from a request" in withContext(url = "/views", hostAddress = "192.1.1.1", hostPort = 8000) { webContext =>
+      webContext.getRemoteAddr shouldEqual "192.1.1.1"
     }
 
-    "be able to get the request http method" in withContext() { webContext =>
+    "get the request http method" in withContext() { webContext =>
       webContext.getRequestMethod shouldEqual "GET"
     }
 
-    "be able to get server name and port from the request" in withContext(hostAddress = "localhost:8000") { webContext =>
-      webContext.getServerName shouldEqual "localhost"
-      webContext.getServerPort shouldEqual "8000"
+    "get server name and port from the request" in withContext(hostAddress = "192.1.1.1", hostPort = 8000) { webContext =>
+      webContext.getServerName shouldEqual "192.1.1.1"
+      webContext.getServerPort shouldEqual 8000
     }
 
-    "be able to get the uri scheme" in withContext() { webContext =>
+    "get the uri scheme" in withContext() { webContext =>
       webContext.getScheme shouldEqual "http"
     }
 
-    "be able to get the request parameters or an empty string when no such parameter exists" in withContext(url = "www.stackstate.com/views?v=1234") { webContext =>
-      webContext.getRequestParameter("v") shouldEqual "1"
+    "get the request parameters or an empty string when no such parameter exists" in withContext(url = "www.stackstate.com/views?v=1234") { webContext =>
+      webContext.getRequestParameter("v") shouldEqual "1234"
       webContext.getRequestParameter("p") shouldEqual ""
     }
 
-    "be able to get/set request attributes" in withContext() { webContext =>
+    "get/set request attributes" in withContext() { webContext =>
       webContext.setRequestAttribute("foo", "bar")
       webContext.getRequestAttribute("foo") shouldEqual "bar"
     }
 
-    "be able to get/set response content" in withContext() { webContext =>
+    "get/set response content" in withContext() { webContext =>
       webContext.writeResponseContent("content")
-      webContext.getRequestContent shouldEqual "content"
+      webContext.getChanges.content shouldEqual "content"
+    }
+
+    "know if a url is secure" in withContext(scheme = "https") { webContext =>
+      webContext.isSecure shouldEqual true
     }
   }
 
@@ -66,11 +70,11 @@ class AkkaHttpWebContextTest extends WordSpecLike with Matchers {
     cookies: List[Cookie] = List.empty,
     url: String = "",
     scheme: String = "http",
-    hostAddress: String = "")(f: AkkaHttpWebContext => Unit): Unit = {
+    hostAddress: String = "",
+    hostPort: Int = 0)(f: AkkaHttpWebContext => Unit): Unit = {
     val parsedHeaders: List[HttpHeader] = requestHeaders.map { case (k, v) => HttpHeader.parse(k, v) }.collect { case Ok(header, _) => header }
     val completeHeaders: List[HttpHeader] = parsedHeaders ++ cookies
-    val uri = Uri(url).withScheme(scheme).withHost(hostAddress)
-
+    val uri = Uri(url).withScheme(scheme).withAuthority(hostAddress, hostPort)
     val request = HttpRequest(uri = uri, headers = completeHeaders)
 
     f(AkkaHttpWebContext(request))
