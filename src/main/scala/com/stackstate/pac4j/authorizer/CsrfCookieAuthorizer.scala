8c893059e5ca0312f341516523f6cefb4e666fee
Carlos Rocha <crocha@stackstate.com>
2022-08-05 12:57:54 +0100

    STAC-17136. Added two cookies for backwards compability for the csrf cookie.

--- src/main/scala/com/stackstate/pac4j/authorizer/CsrfCookieAuthorizer.scala
+++ src/main/scala/com/stackstate/pac4j/authorizer/CsrfCookieAuthorizer.scala
@@ -14,18 +14,30 @@ object CsrfCookieAuthorizer {
 
   def apply(context: AkkaHttpWebContext, maxAge: Option[FiniteDuration]): AkkaHttpWebContext = {
     val token = UUID.randomUUID.toString
+
+    val cookieWithDomain = createCookie(token, maxAge)
+    cookieWithDomain.setDomain(context.getServerName)
+
+    val cookieWithoutDomain = createCookie(token, maxAge)
+
+    context.setRequestAttribute(Pac4jConstants.CSRF_TOKEN, token)
+    context.getSessionStore.set(context, Pac4jConstants.CSRF_TOKEN, token)
+
+    // previous versions set both cookies. This change is to keep it backwards compatible.
+    context.addResponseCookie(cookieWithDomain)
+    context.addResponseCookie(cookieWithoutDomain)
+    context
+  }
+
+
+  def createCookie(token: String, maxAge: Option[FiniteDuration]) = {
     val cookie = new Cookie(Pac4jConstants.CSRF_TOKEN, token)
     cookie.setPath(CookiePath)
 
     maxAge.map(_.toSeconds.toInt).foreach {
       cookie.setMaxAge
     }
-
-    context.setRequestAttribute(Pac4jConstants.CSRF_TOKEN, token)
-    context.getSessionStore.set(context, Pac4jConstants.CSRF_TOKEN, token)
-    context.addResponseCookie(cookie)
-
-    context
+    cookie
   }
 
 }
